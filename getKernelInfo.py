a3e40967d2fb5df3b1132654268fdf2c835cd30a
YourSandwich <norbert23pl@gmail.com>
2021-05-01 22:26:27 +0200

    Deprication fix

--- getKernelInfo.py
+++ getKernelInfo.py
@@ -18,7 +18,7 @@ KernelVerList = []
 for m in project_href[::2]:   
     KernelURL.append("https://archive.archlinux.org/packages/l/linux/"+m)
 
-## Making complicated stuff to extract only the Kernal Version from the WebScrap
+## Making complicated stuff to extract only the Kernel Version from the WebScrap
 for t in project_href[::2]:
     KernelVer = os.popen("echo " + t + " | cut -d'.' -f1,2,3 | sed 's/.arc//g' | sed 's/-x86_64//g' | sed 's/.ar//g' | sed 's/h//g'")
     for i in KernelVer.read().split('\n')[::2]:   
