d70ac88d1ba81d254908da8e0c5be2092e7bf511
YourSandwich <norbert23pl@gmail.com>
2022-05-22 12:50:47 +0200

    A simple update after a year
    why did i use two foreach loops?

--- getKernelInfo.py
+++ getKernelInfo.py
@@ -14,12 +14,11 @@ del project_href[0]
 
 KernelURL = []
 KernelVerList = []
-
-for m in project_href[::2]:   
-    KernelURL.append("https://archive.archlinux.org/packages/l/linux/"+m)
-
+ 
+    
 ## Making complicated stuff to extract only the Kernel Version from the WebScrap
 for t in project_href[::2]:
+    KernelURL.append("https://archive.archlinux.org/packages/l/linux/"+t)
     KernelVer = os.popen("echo " + t + " | cut -d'.' -f1,2,3 | sed 's/.arc//g' | sed 's/-x86_64//g' | sed 's/.ar//g' | sed 's/h//g'")
     for i in KernelVer.read().split('\n')[::2]:   
         KernelVerList.append(i)
